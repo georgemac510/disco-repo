880d8ec88dc346e3c5692704a358c61b6937fe0a
georgemac510 <georgemac510@gmail.com>
2021-11-11 11:23:18 -0500

    First commit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-    trailingSlash: true,
-    exportPathMap: function () {
-      return {
-        '/': { page: '/' }
-      };
-    }
-};
-=======
   trailingSlash: true,
   exportPathMap: function () {
     return {
@@ -15,4 +6,3 @@ module.exports = {
     };
   }
 };
->>>>>>> 6e01e608c1ce2f6b902cf49c0250288d0e47adef
